11bb099fea2272d3079e1b818b0a82a7e127d1d3
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-16 14:56:43 -0700

    Profile image on main page

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,7 +3,12 @@ import "./main.css"
 export default function Main() {
     return (
         <div className="main" id="main">
-    
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/Me.JPG"></img>
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
