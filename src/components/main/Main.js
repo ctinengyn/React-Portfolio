9e13176ba0272f63c1872db6732835f25026ad06
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-16 23:49:59 -0700

    Final styling for main page

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,15 +2,16 @@ import "./main.css"
 import { init } from 'ityped'
 import { useEffect, useRef } from "react"
 
-// console.log(textRef)
-
 export default function Main() {
 
+    // Styles for "ityed"
     const textRef = useRef();
 
     useEffect(() => {
         init(textRef.current, { 
             showCursor: false, 
+            backDelay: 1700,
+            backSpeed: 70,
             strings: ['Developer', 'Programmer', "Content Creater" ]
         });
     }, [])
@@ -30,7 +31,7 @@ export default function Main() {
                 <div className="wrapper">
                     <h2>Hi there, I'm</h2>
                     <h1>Christine Nguyen</h1>
-                    <h3>Developer <div class="pro" ref={textRef}></div></h3>
+                    <h3><div class="pro" ref={textRef}></div></h3>
                 </div>
                 <a href="#works">
                     <div class="down" scr="assets/down.png" alt="Down arrow"/>
