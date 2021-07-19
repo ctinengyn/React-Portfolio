bd3f4e560ab4f05d6dec780c5287f1d5424e9a7f
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-18 21:44:51 -0700

    Live links for each work example when user clicks on image

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -9,33 +9,52 @@ import Work from "../../assets/workschedule.JPG"
 export default function Works() {
 
     return (
+
+        // Work/Project image examples with live links
         <div className="works" id="worksid">
             <h1 className="workh1">Portfolio</h1>
 
             <div className="workcontainer">
                 <div className="workfitem">
-                    <img className="example1" src={Fitness} alt="Workout Tracker Example"/>
+                    <a href="https://enigmatic-lowlands-43601.herokuapp.com/?id=60e9d24ac99c7100153ac36c"> 
+                    <img className="example1" src={Fitness} alt="Workout Tracker Example" />
                     <h3 className="workh3">Fitness Tracker</h3>
-                    </div>
-                    <div className="workitem">
+                    </a>
+                </div>
+
+                <div className="workitem">
+                    <a href="https://dry-caverns-13937.herokuapp.com/">
                     <img className="example1" src={Budget} alt="Workout Tracker Example"/>
-                    <h3 className="workh3">Fitness Tracker</h3>
-                    </div>
+                    <h3 className="workh3">Budget Tracker</h3>
+                    </a>
+                </div>
+
                 <div className="workitem">
+                    <a href="https://whos-the-pokemon.herokuapp.com/"> 
                     <img className="example1" src={Pokemon} alt="Workout Tracker Example"/>
-                    <h3 className="workh3">Fitness Tracker</h3>
-                    </div>
+                    <h3 className="workh3">Who's That Pokemon?</h3>
+                    </a>
+                </div>
+
                 <div className="workitem">
+                    <a href="https://whispering-woodland-84908.herokuapp.com/">
                     <img className="example1" src={Note} alt="Workout Tracker Example"/>
-                    <h3 className="workh3">Fitness Tracker</h3>
-                    </div>
+                    <h3 className="workh3">Note Taker</h3>
+                    </a>
+                </div>
+
                 <div className="workitem">
+                    <a href="https://github.com/ctinengyn/Team-Profile-Generator">
                     <img className="example1" src={Team} alt="Workout Tracker Example"/>
-                    <h3 className="workh3">Fitness Tracker</h3>
-                    </div>
+                    <h3 className="workh3">Team Profile</h3>
+                    </a>
+                </div>
+
                 <div className="workitem">
+                    <a href="https://github.com/ctinengyn/Work-Day-Scheduler">
                     <img className="example1" src={Work} alt="Workout Tracker Example"/>
-                    <h3 className="workh3">Fitness Tracker</h3>
+                    <h3 className="workh3">Work Day Scheduler</h3>
+                    </a>
                 </div>
             </div>
         </div>
