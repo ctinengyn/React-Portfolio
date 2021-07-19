c2c4ad3bb0ecc49b2325e58a72d4e5fe200a0631
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-19 11:35:45 -0700

    Button styles and message after submitting

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,18 +1,34 @@
 import "./contact.css"
 import Mobile from "../../assets/mobile.png"
+import React, { useState } from 'react';
 
 export default function Contact() {
+
+    // When user clicks button a message will come up "Thank you!"
+    const [message, setMessage] = useState(false)
+
+    // Stops the page from refreshing when clicking button "send"
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        setMessage(true)
+    }
+
     return (
+
+        // Styles for form container
         <div className="contact" id="contact">
             <div className="contactleft">
                 <img className="mobileimg" src={Mobile} alt=""></img>
             </div>
                 <div className="contactright">
                     <h2 className="contacth2">Contact</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="Email"></input>
                     <textarea placeholder="Message"></textarea>
                     <button type="submit">Send</button>
+                    
+                    {/* Message for button */}
+                    {message && <p>Thank you!</p>}
                 </form>
             </div>
         </div>
