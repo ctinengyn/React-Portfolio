9828d5e3ca3e0a248b8ab282dd22d3ca83675dc8
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-19 11:17:26 -0700

    Update

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,17 +4,17 @@ import Mobile from "../../assets/mobile.png"
 export default function Contact() {
     return (
         <div className="contact" id="contact">
-           <div className="contactleft">
-               <img className="mobileimg" src={Mobile} alt=""></img>
-           </div>
-           <div className="contactright">
-               <h2 className="contacth2">Contact</h2>
-               <form>
-                   <input type="text" placeholder="Email"></input>
-                   <textarea placeholder="Message"></textarea>
-                   <button type="submit">Send</button>
-               </form>
-           </div>
+            <div className="contactleft">
+                <img className="mobileimg" src={Mobile} alt=""></img>
+            </div>
+                <div className="contactright">
+                    <h2 className="contacth2">Contact</h2>
+                <form>
+                    <input type="text" placeholder="Email"></input>
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
     )
 }
