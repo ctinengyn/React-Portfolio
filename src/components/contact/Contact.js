76b3a3134af35aae259404f9058208c8a30983c6
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-19 01:31:06 -0700

    Changing textarea and placeholder styles

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,12 +5,14 @@ export default function Contact() {
     return (
         <div className="contact" id="contact">
            <div className="contactleft">
-               <img src={Mobile} alt=""></img>
+               <img className="mobileimg" src={Mobile} alt=""></img>
            </div>
            <div className="contactright">
                <h2 className="contacth2">Contact</h2>
                <form>
                    <input type="text" placeholder="Email"></input>
+                   <textarea placeholder="Message"></textarea>
+                   <button type="submit">Send</button>
                </form>
            </div>
         </div>
